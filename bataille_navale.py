ff5237d7f8338b0d9f15c7a2ceb55b32755e22c0
kamelia-slimani <slimani44u@etu.univ-lorraine.fr>
2023-10-11 09:37:05 +0200

    jeu basique fonctionnel

--- bataille_navale.py
+++ bataille_navale.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 import random
-import tkinter as tk
-from tkinter import messagebox
+import pygame
+import sys
 
-TAILLE_GRILLE = 5
+# Initialiser Pygame
+pygame.init()
+
+TAILLE_GRILLE = 8  # Augmenter la taille de la grille
+CASE_SIZE = 60    # Reduire la taille des cases
+
 grille = [['O' for _ in range(TAILLE_GRILLE)] for _ in range(TAILLE_GRILLE)]
 
 for _ in range(3):
@@ -28,7 +33,55 @@ def tirer(x, y):
         grille[x][y] = 'M'
         return False
 
-def tirer_case():
-    x = int(entree_x.get())
-    y = int(entree_y.get
+def tirer_case(x, y):
+    if grille[x][y] == 'X':
+        print("Touché !")
+        grille[x][y] = '*'
+        return True
+    else:
+        print("Manqué !")
+        grille[x][y] = 'M'
+        return False
 
+def creer_interface():
+    # Créer la fenêtre
+    fenetre = pygame.display.set_mode((TAILLE_GRILLE*CASE_SIZE, TAILLE_GRILLE*CASE_SIZE))
+    pygame.display.set_caption("Bataille Navale")
+
+    # Charger l'image de fond
+    fond = pygame.image.load('fond.jpeg')
+    fond = pygame.transform.scale(fond, (TAILLE_GRILLE*CASE_SIZE, TAILLE_GRILLE*CASE_SIZE))
+    fenetre.blit(fond, (0, 0))
+
+    # Charger l'image du bateau
+    bateau = pygame.image.load('bateau.png')
+    bateau = pygame.transform.scale(bateau, (CASE_SIZE, CASE_SIZE))
+
+    # Dessiner les navires
+    for i in range(TAILLE_GRILLE):
+        for j in range(TAILLE_GRILLE):
+            if grille[i][j] == 'X':
+                fenetre.blit(bateau, (j*CASE_SIZE, i*CASE_SIZE))
+
+    pygame.display.flip()
+
+    # Boucle de jeu
+    while any('X' in ligne for ligne in grille):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                x //= CASE_SIZE
+                y //= CASE_SIZE
+
+                if x < 0 or x >= TAILLE_GRILLE or y < 0 or y >= TAILLE_GRILLE:
+                    print("Coordonnées invalides, réessayez.")
+                    continue
+
+                if tirer_case(y, x):
+                    break
+
+creer_interface()
